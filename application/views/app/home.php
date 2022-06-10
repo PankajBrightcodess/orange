3515d703f99f5a3e451bec23770adc85e276e5cc
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-06-10 10:48:00 +0530

    update

--- application/views/app/home.php
+++ application/views/app/home.php
@@ -103,7 +103,7 @@
                               <span class="badge badge-primary" style="margin-left: 88px;"><?php echo $value['discount'].'%';?></span>
                              <a class="product-thumbnail d-block" href="<?= base_url('app_control/singleproduct/?id='.$value['id']);?>"><img class="mb-2" src="<?= base_url($value['image']);?>" alt="">
                              </a>
-                             <a class="product-title d-block" href="<?= base_url('app_control/singleproduct');?>"><?php echo $value['product_name'];?></a>
+                             <a class="product-title d-block" href="<?= base_url('app_control/singleproduct/?id='.$value['id']);?>"><?php echo $value['product_name'];?></a>
                              <p class="sale-price"><?php echo $value['offer_price'];?><span><?php echo $value['price'];?></span></p>
                             <!--  <div class="product-rating"><i class="lni lni-star-filled"></i><i class="lni lni-star-filled"></i><i class="lni lni-star-filled"></i><i class="lni lni-star"></i><i class="lni lni-star"></i></div> -->
                              <a class="btn btn-success btn-sm" href="#"><i class="lni lni-plus"></i></a>
@@ -147,10 +147,10 @@
 
                                 <span class="badge badge-success"><?php echo $value['brand'];?></span>
                                 <!-- Wishlist Button --><!-- <a class="wishlist-btn" href="#"><i class="lni lni-heart"></i></a> -->
-                                <!-- Thumbnail --><a class="product-thumbnail d-block" href="<?= base_url('app_control/singleproduct');?>"><img src="<?= base_url($value['image'])?>" alt=""></a>
+                                <!-- Thumbnail --><a class="product-thumbnail d-block" href="<?= base_url('app_control/singleproduct?id='.$value['id']);?>"><img src="<?= base_url($value['image'])?>" alt=""></a>
                               </div>
                               <div class="product-description">
-                                <!-- Product Title --><a class="product-title d-block" href="<?= base_url('app_control/singleproduct');?>"><?php echo $value['product_name'];?></a>
+                                <!-- Product Title --><a class="product-title d-block" href="<?= base_url('app_control/singleproduct?id='.$value['id']);?>"><?php echo $value['product_name'];?></a>
                                 <!-- Price -->
                                 <p class="sale-price"><i class="lni lni-dollar"></i><?php echo $value['offer_price'];?><span><?php echo $value['price'];?></span></p>
                                 <!-- Rating -->
@@ -202,10 +202,10 @@
                             <!-- Badge--><span class="badge badge-warning custom-badge"><i class="lni lni-star"></i></span>
                             <div class="product-thumbnail-side">
                               <!-- Wishlist Button --><a class="wishlist-btn shadow-lg border" href="#"><?= $value['discount'].'%';?></a>
-                              <!-- Thumbnail --><a class="product-thumbnail d-block" href="<?= base_url('app_control/singleproduct');?>"><img src="<?= base_url($value['image'])?>" alt=""></a>
+                              <!-- Thumbnail --><a class="product-thumbnail d-block" href="<?= base_url('app_control/singleproduct?id='.$value['id']);?>"><img src="<?= base_url($value['image'])?>" alt=""></a>
                             </div>
                             <div class="product-description">
-                              <!-- Product Title --><a class="product-title d-block" href="<?= base_url('app_control/singleproduct');?>"><?= $value['product_name'];?></a>
+                              <!-- Product Title --><a class="product-title d-block" href="<?= base_url('app_control/singleproduct?id='.$value['id']);?>"><?= $value['product_name'];?></a>
                               <!-- Price -->
                               <p class="sale-price"><?= $value['offer_price'];?><span><?= $value['price']?></span></p>
                             </div>
